fd78f0cf89a0ba3726f6ff92578f2caf8a4c2d9e
ScrublordBG <megata10@gmail.com>
2018-04-26 16:29:59 +0300

    Updated Hero class

--- src/Game/Hero/Hero.java
+++ src/Game/Hero/Hero.java
@@ -1,9 +1,32 @@
 package Game.Hero;
 
+import Game.Items.Item;
+import Game.Items.Weapon;
+import Game.Skill;
+import Game.Stat;
+
+import java.util.ArrayList;
+
 public abstract class Hero {
-    int hp;
-    int xp;
-    int level;
+    int hp=100;
+    int xp=0;
+    int level=1;
+    int defence=0;
+    int stat=1;
+    Stat primaryStat;
     String name;
+    Weapon weapon;
+    ArrayList<Item> inventory;
+    ArrayList<Skill> skills;
+
+    void learnSkill(){
+
+    }
+
+    void castSkill(){
+
+    }
+
+
 
 }
