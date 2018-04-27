d442ef84bb347577546b46f2132f228b1162b615
Veselin Georgiev <veselin_georgiev@yahoo.com>
2018-04-27 18:12:47 +0300

    Dungeon update adding methods and fields

--- src/Game/Location/Dungeon.java
+++ src/Game/Location/Dungeon.java
@@ -12,7 +12,7 @@ public class Dungeon extends Location {
   int gold;
   int XP;
 
-  ArrayList<Enemy> enemies = new ArrayList<>();
+ private ArrayList<Enemy> enemies = new ArrayList<>();
 
   public Dungeon(Difficulty difficulty) {
 
@@ -20,27 +20,44 @@ public class Dungeon extends Location {
       case EASY:
         coefficient = 0.8;
         enemiesNumbers = 5;
+        gold = 100;
+        XP = 50;
         break;
       case MEDIUM:
         coefficient = 1.0;
         enemiesNumbers = 10;
+        gold = 300;
+        XP = 200;
         break;
       case HARD:
         coefficient = 1.2;
         enemiesNumbers = 20;
+        gold = 500;
+        XP = 300;
         break;
       default:
         coefficient = 1.0;
         enemiesNumbers = 10;
         break;
     }
-
+    enemies = generateEnemies();
   }
 
-  void generateEnemies() {
-    for (int i = 0; i < enemiesNumbers; i++) {
-      enemies.add(new Enemy((int)coefficient));
+  public int getGold() {
+    return gold;
+  }
 
-    }
+  public int getXP() {
+    return XP;
+  }
+
+  public ArrayList<Enemy> getEnemies() {
+    return enemies;
+  }
+
+  private ArrayList generateEnemies() {
+    for (int i = 0; i < enemiesNumbers; i++) {
+      enemies.add(new Enemy((int) coefficient));
+    }return enemies;
   }
 }
