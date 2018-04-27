e0f2ec1bf691490c1daae25490d94a276db943f1
bilyana <bibby.georgieva@gmail.com>
2018-04-27 17:51:16 +0300

    Moved a lot of main functions to different methods in Engine

--- src/Game/Engine.java
+++ src/Game/Engine.java
@@ -9,59 +9,86 @@ import Game.Items.Item;
 import Game.Location.Enemy;
 
 import java.util.HashSet;
+import java.util.Scanner;
+
+import static Game.Message.*;
+
 public class Engine {
     HashSet<Skill> allSkills = new HashSet<>();
     HashSet<Item> allItems = new HashSet<>();
     HashSet<Enemy> allEnemies = new HashSet<>();
-    HashSet<Hero> heroes = new HashSet<>();
-    Hero currentHero;
+    private HashSet<Hero> heroes = new HashSet<>();
+    private Hero currentHero;
+    Scanner in = new Scanner(System.in);
 
     Engine(){
+        start();
+    }
 
-    };
+    private void start() {
+        System.out.println(startMessage);
+        String name = in.nextLine();
+        System.out.println(chooseHeroType);
+        addHero(name, getHeroTypes(in.nextInt()));
+        System.out.println(firstHeroAdded);
+        showMyStats();
+        System.out.println(firstDungeon);
+    }
 
-    public void addHero(String name, PlayerType type){
-        if(type==PlayerType.WARRIOR){
+    private PlayerType getHeroTypes(int number) {
+        switch (number) {
+            case 1:
+                return PlayerType.WARRIOR;
+            case 2:
+                return PlayerType.MAGE;
+            case 3:
+                return PlayerType.ARCHER;
+        }
+        return PlayerType.ARCHER;
+    }
+
+    private void addHero(String name, PlayerType type) {
+        if (type == PlayerType.WARRIOR) {
             currentHero = new Warrior(name);
             heroes.add(currentHero);
-        } else if(type==PlayerType.MAGE){
+        } else if (type == PlayerType.MAGE) {
             currentHero = new Mage(name);
             heroes.add(currentHero);
-        } else if(type==PlayerType.ARCHER){
+        } else if (type == PlayerType.ARCHER) {
             currentHero = new Archer(name);
             heroes.add(currentHero);
         }
     }
 
-    public void showMyHeroes(){
+    private void showMyHeroes() {
         heroes.forEach(System.out::println);
     }
 
-    public void chooseCurrentHero(String name) throws NoSuchHero{
-            for (Hero hero:heroes){
-                if(hero.getName().equals(name)){
-                    currentHero = hero;
-                    System.out.printf("Hero is set to %s", hero);
-                    return;
-                }
+    private void chooseCurrentHero(String name) throws NoSuchHero {
+        for (Hero hero : heroes) {
+            if (hero.getName().equals(name)) {
+                currentHero = hero;
+                System.out.printf("Hero is set to %s", hero);
+                return;
             }
-            throw new NoSuchHero();
+        }
+        throw new NoSuchHero();
     }
 
-    public void showMyStats(){
-        System.out.println("HP: "+currentHero.getCurrentHP());
-        System.out.println("XP: "+ currentHero.getXp());
-        System.out.println("Level: "+ currentHero.getLevel());
-        System.out.println("XP until next level: "+ currentHero.getXpUntilNextlevel());
-        System.out.println("Defence: "+ currentHero.getDefence());
-        System.out.println("Damage: "+ currentHero.getDamage());
-        System.out.println("Gold: "+ currentHero.getGold());
-        System.out.println("Weapon: "+ currentHero.getWeapon().toString());
-        System.out.println("Helmet: "+ currentHero.getHelmet().toString());
-        System.out.println("Vest: "+ currentHero.getVest().toString());
+    private void showMyStats() {
+        System.out.println("HP: " + currentHero.getCurrentHP());
+        System.out.println("XP: " + currentHero.getXp());
+        System.out.println("Level: " + currentHero.getLevel());
+        System.out.println("XP until next level: " + currentHero.getXpUntilNextlevel());
+        System.out.println("Defence: " + currentHero.getDefence());
+        System.out.println("Damage: " + currentHero.getDamage());
+        System.out.println("Gold: " + currentHero.getGold());
+        System.out.println("Weapon: " + currentHero.getWeapon().toString());
+        System.out.println("Helmet: " + currentHero.getHelmet().toString());
+        System.out.println("Vest: " + currentHero.getVest().toString());
     }
 
-    public void showMyInventory(){
+    private void showMyInventory() {
         currentHero.getInventory().entrySet().forEach(System.out::println);
     }
 }
--- src/Game/Main.java
+++ src/Game/Main.java
@@ -12,22 +12,26 @@ public class Main {
     Scanner in = new Scanner(System.in);
     Engine engine = new Engine();
 
-    System.out.println("Hello and welcome to Team IRONMAN's video game!\n \nTo start a new game you must first choose a name for your hero by typing it here: ");
-    String name = in.nextLine();
-    System.out.println("Great! Now choose the type of your hero by pressing 1 for Warrior, 2 for Mage or 3 for Archer.");
-    switch (in.nextInt()) {
-      case 1:
-        engine.addHero(name, PlayerType.WARRIOR);
-        break;
-      case 2:
-        engine.addHero(name, PlayerType.MAGE);
-        break;
-      case 3:
-        engine.addHero(name, PlayerType.ARCHER);
-        break;
-    }
-    System.out.println("That's an awesome choice! Here are your default stats, which you can improve on as you play.\n");
-    engine.showMyStats();
-    System.out.println("\nNow it's time for the real fun to begin. You don't have any gold yet, so let's go to a dungeon to win some.\nThis time we will start with an easy one but from now on you will be able to choose the difficulty yourself!\n");
+//    System.out.println("Hello and welcome to Team IRONMAN's video game!\n \nTo start a new game you must first choose a name for your hero by typing it here: ");
+//    String name = in.nextLine();
+//    System.out.println("Great! Now choose the type of your hero by pressing 1 for Warrior, 2 for Mage or 3 for Archer.");
+//    switch (in.nextInt()) {
+//      case 1:
+//        engine.addHero(name, PlayerType.WARRIOR);
+//        break;
+//      case 2:
+//        engine.addHero(name, PlayerType.MAGE);
+//        break;
+//      case 3:
+//        engine.addHero(name, PlayerType.ARCHER);
+//        break;
+//    }
+//    System.out.println("That's an awesome choice! Here are your default stats, which you can improve on as you play.\n");
+//    engine.showMyStats();
+//    System.out.println("\nNow it's time for the real fun to begin. You don't have any gold yet, so let's go to a dungeon to win some.\nThis time we will start with an easy one but from now on you will be able to choose the difficulty yourself!\n");
+
+
+    //go to easy dungeon - create fight method in engine
   }
+
 }
