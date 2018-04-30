c28d9a3b09022298e93bd0538d5ca94e1bdb8348
Veselin Georgiev <veselin_georgiev@yahoo.com>
2018-04-30 15:39:05 +0300

    Adding Town Class and Shops

--- src/Game/Location/Shop/Shop.java
+++ src/Game/Location/Shop/Shop.java
@@ -1,12 +1,49 @@
 package Game.Location.Shop;
 
-import Game.Items.Item;
+import Game.Items.*;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Shop {
-    ArrayList<Item> inventory;
-    
+  HashSet<Item> inventory;
 
 
+  public Shop(String type) {
+    inventory = new HashSet<>();
+    generateRandomItems(type);
+
+  }
+
+  void generateRandomItems(String item) {
+    switch (item) {
+      case "Weapon":
+        for (int i = 0; i < 5; i++) {
+          inventory.add(new Weapon());
+        }
+        break;
+      case "Helmet":
+        for (int i = 0; i < 5; i++) {
+          inventory.add(new Helmet());
+        }
+        break;
+      case "Vest":
+        for (int i = 0; i < 5; i++) {
+          inventory.add(new Vest());
+        }
+        break;
+      case "Potion":
+        for (int i = 0; i < 5; i++) {
+          inventory.add(new HPPotion());
+        }
+        break;
+      default:
+        break;
+    }
+
+
+  }
+
+  public HashSet<Item> getInventory() {
+    return inventory;
+  }
 }
---
+++ src/Game/Location/Town.java
@@ -0,0 +1,23 @@
+package Game.Location;
+
+import Game.Location.Shop.Shop;
+
+import java.util.HashSet;
+
+public class Town {
+
+  private HashSet<Shop> shops = new HashSet<>();
+
+  public Town() {
+    shops.add(new Shop("Weapon"));
+    shops.add(new Shop("Helmet"));
+    shops.add(new Shop("Vest"));
+    shops.add(new Shop("Potion"));
+
+  }
+
+  public HashSet<Shop> getShops() {
+    return shops;
+  }
+
+}
